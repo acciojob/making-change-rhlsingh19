3a690b5284e6cb5457cf586adea4d4ebebb2ac6d
rhlsingh19 <145037560+rhlsingh19@users.noreply.github.com>
2024-01-03 12:42:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 const makeChange = (c) => {
   // your name here
-};
+	const coins = {
+    q: 0,
+    d: 0,
+    n: 0,
+    p: 0
+  };
+
+  while (amount > 0) {
+    if (amount >= 25) {
+      coins.q++;
+      amount -= 25;
+    } else if (amount >= 10) {
+      coins.d++;
+      amount -= 10;
+    } else if (amount >= 5) {
+      coins.n++;
+      amount -= 5;
+    } else {
+      coins.p += amount;
+      amount = 0;
+    }
+  }
+
+  return coins;
+}
+console.log(makeChange(47)); // Output: { "q": 1, "d": 2, "n": 0, "p": 2 }
+console.log(makeChange(24)); 
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
